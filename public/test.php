13b2bc0e1c6f2aef0dd35ca76006c68f5fb06caa
chunzai <571115298@qq.com>
2018-09-21 13:47:36 +0800

    fix-issue-AddSite.php

--- public/test.php
+++ public/test.php
@@ -2,7 +2,7 @@
 
 function test ()
 {
-    $res = file_get_contents("http://mrw.so/api.php?url=http%3A%2F%2Fwww.baidu.com");
+    $res = file_get_contents("http://mrw.so/api.php?url=http%3A%2F%2Fwww.baidu.com");echo 123;
     echo $res;
     $a = substr($res, 0, 4);
     if ( $a == 'http') {
