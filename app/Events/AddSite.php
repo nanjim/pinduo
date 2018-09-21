7c0d785da8d5f6ab7e126493754c88e596b1f5ed
chunzai <571115298@qq.com>
2018-09-21 11:06:03 +0800

    fix-issue-AddSite.php

--- app/Events/AddSite.php
+++ app/Events/AddSite.php
@@ -49,10 +49,10 @@ class AddSite
     function getShortLink($url)
     {
         $url_short =  file_get_contents("http://mrw.so/api.php?url=".$url);
-        $is_url = preg_match("/http:\/\//", $url_short);
-        if (!$is_url) {
-            return $this->getShortLink($url);
-        }
+//        $is_url = preg_match("/http:\/\//", $url_short);
+//        if (!$is_url) {
+//            return $this->getShortLink($url);
+//        }
         return $url_short;
     }
 }
