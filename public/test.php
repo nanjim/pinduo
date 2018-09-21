7bf886c9fe4443f5b0ee27938c3ed4c2b5df1191
chunzai <571115298@qq.com>
2018-09-21 13:38:40 +0800

    fix-issue-AddSite.php

--- public/test.php
+++ public/test.php
@@ -3,7 +3,7 @@
 function test ()
 {
     $res = file_get_contents("http://mrw.so/api.php?url=http%3A%2F%2Fwww.baidu.com");
-    if (substr($res, 0, 4) = 'http') {
+    if (substr($res, 0, 4) == 'http') {
         echo $res;
     } else {
         test();
