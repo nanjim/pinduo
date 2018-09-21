18e35293f787418ce3f0238dfd40220a4b200860
chunzai <571115298@qq.com>
2018-09-21 12:11:18 +0800

    fix-issue-AddSite.php

--- public/test.php
+++ public/test.php
@@ -1,3 +1,3 @@
 <?php
 
-echo file_get_contents("http://mrw.so/api.php?url=http://mobile.yangkeduo.com/goods2.html?goods_id=54023743");
+echo file_get_contents("http://mrw.so/api.php?url='http://mobile.yangkeduo.com/goods2.html?goods_id=54023743'");
