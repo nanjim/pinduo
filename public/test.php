823d64c7574bf30525b2ab594ce7573bb52ec9bb
chunzai <571115298@qq.com>
2018-09-21 13:38:06 +0800

    fix-issue-AddSite.php

--- public/test.php
+++ public/test.php
@@ -1,3 +1,14 @@
 <?php
 
-echo file_get_contents("http://mrw.so/api.php?url=http%3A%2F%2Fwww.baidu.com");
+function test ()
+{
+    $res = file_get_contents("http://mrw.so/api.php?url=http%3A%2F%2Fwww.baidu.com");
+    if (substr($res, 0, 4) = 'http') {
+        echo $res;
+    } else {
+        test();
+    }
+
+}
+
+
