ade2d0adfd9d30e5e553042fdcf484af8298fe60
chunzai <571115298@qq.com>
2018-09-27 14:00:25 +0800

    style of goods detail

--- resources/views/index/layout/foot.blade.php
+++ resources/views/index/layout/foot.blade.php
@@ -1,11 +1,10 @@
 
-<div class="foot-wrap col-md-12 text-center" style="height: 2rem;line-height: 2rem">
+<div class="foot-wrap col-md-12 text-center" style="height: 2rem;line-height: 2rem; width: 100vw;padding: 0;">
 	<a href="{{route('entry')}}">首页 |</a>
 	<a href="{{route('index.myprofile.spread')}}">放单后台 |</a>
 	<a href="{{route('index.myprofile.baseData')}}">个人中心 |</a>
 	<a href="{{route('index.myprofile.myStar')}}">我的收藏</a>
-</div>
-<div class="text-center" style="background: white;height: 2rem;line-height: 2rem">
-	<span style="font-weight: 600">CopyRight 2016-{{date('Y', time())}} |  惠麦客 hmkapp.com | 浙ICP备17052092号-2
-</span>
+	<div class="text-center" style="background: white;height: 2rem;line-height: 2rem">
+		<span style="font-weight: 600">CopyRight 2016-{{date('Y', time())}} |  惠麦客 hmkapp.com | 浙ICP备17052092号-2</span>
+	</div>
 </div>
