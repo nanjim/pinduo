a2f779e5aa4873ae9258fefea56f846cabce9a59
chunzai <571115298@qq.com>
2018-10-09 08:51:56 +0800

    change header

--- config/common.php
+++ config/common.php
@@ -5,19 +5,19 @@ return [
 	'index_register_tpl' => 'SMS_140045157',
     'nav' =>[
         '0' => [
-            'name' => '实时爆款',
+            'name' => '拼多多',
             'index' => 1,
             'route' => 'entry'
         ],
-        '2' => [
-            'name' => '今日榜单',
-            'index' => 2,
-            'route' => 'entry'
-        ],
-        '3' => [
-            'name' => '我的网站',
-            'index' => 3,
-            'route' => 'entry'
-        ],
+        // '2' => [
+        //     'name' => '今日榜单',
+        //     'index' => 2,
+        //     'route' => 'entry'
+        // ],
+        // '3' => [
+        //     'name' => '我的网站',
+        //     'index' => 3,
+        //     'route' => 'entry'
+        // ],
     ],
 ];
